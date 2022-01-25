6aaafe8c00f18ae4416b0b03dd9c77817d316f86
juftin <juftin@juftin.com>
2022-01-24 23:23:22 -0700

    Unit Tests

--- tests/command_line_test.sh
+++ tests/command_line_test.sh
@@ -10,3 +10,5 @@ camply campsites --campground 252037 --start-date 2022-09-15 --end-date 2022-09-
 camply campsites --yml-config tests/yml/example_search.yml
 camply campsites --campground 232045 --start-date 2022-07-15 --end-date 2022-10-01 --nights 5
 camply campsites --provider yellowstone --start-date 2022-10-10 --end-date 2022-10-16
+camply campsites --campsite 40107 --start-date 2022-09-15 --end-date 2022-09-17
+camply campgrounds --campsite 40107
