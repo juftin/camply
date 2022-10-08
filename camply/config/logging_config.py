22d35a32a400304ddf3c5f061107f356565f78fd
Justin Flannery <juftin@juftin.com>
2022-10-08 00:00:45 -0600

    Universal Option Placement (#134)
    * Universal Argument Placement
    
    * Logging Config
    
    * Logging Config
    
    * --provider
    
    * New Command Line Tests
    
    * New Command Line Tests
    
    * New Command Line Tests
    
    * README.md

--- camply/config/logging_config.py
+++ camply/config/logging_config.py
@@ -5,12 +5,14 @@ Dynamic Logging Configuration
 import logging
 import os
 from os import getenv
-from typing import Optional
+from typing import Optional, Tuple, Union
 
 from rich.logging import RichHandler
 
 
-def get_log_handler(log_level: Optional[int] = None) -> logging.Handler:
+def get_log_handler(
+    log_level: Optional[int] = None,
+) -> Tuple[logging.Handler, Union[int, str]]:
     """
     Determine which logging handler should be used
 
@@ -22,29 +24,26 @@ def get_log_handler(log_level: Optional[int] = None) -> logging.Handler:
 
     Returns
     -------
-    logging.Handler
+    Tuple[logging.Handler, Union[int, str]]
     """
     if log_level is None:
         log_level = logging.getLevelName(getenv("LOG_LEVEL", "INFO").upper())
-
     rich_handler = RichHandler(
         level=log_level,
         rich_tracebacks=True,
         omit_repeated_times=False,
         show_path=False,
     )
-
-    standard_handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(asctime)s [%(levelname)8s]: %(message)s")
-    standard_handler.setFormatter(formatter)
-    standard_handler.setLevel(log_level)
-
+    python_handler = logging.StreamHandler()
+    python_formatter = logging.Formatter("%(asctime)s [%(levelname)8s]: %(message)s")
+    python_handler.setFormatter(python_formatter)
+    python_handler.setLevel(log_level)
     _log_dict = {
         "rich": rich_handler,
-        "python": standard_handler,
+        "python": python_handler,
     }
     log_handler_name = os.getenv("CAMPLY_LOG_HANDLER", "rich")
-    log_handler = _log_dict.get(log_handler_name.lower(), rich_handler)
+    log_handler: logging.Handler = _log_dict.get(log_handler_name.lower(), rich_handler)
     return log_handler, log_level
 
 
@@ -58,18 +57,12 @@ def set_up_logging(log_level: Optional[int] = None) -> None:
         Which logging level should be used. If none is provided the LOG_LEVEL environment
         variable will be used, defaulting to "INFO".
     """
-    kwargs = {}
-    log_handler, handler_level = get_log_handler(log_level=log_level)
+    log_handler, level_to_log = get_log_handler(log_level=log_level)
+    logging.root.handlers = [log_handler]
     if isinstance(log_handler, RichHandler):
-        kwargs["datefmt"] = "[%Y-%m-%d %H:%M:%S]"
-        kwargs["format"] = "%(message)s"
-        level = logging.NOTSET
-    else:
-        level = handler_level
-    logging.basicConfig(
-        level=level,
-        handlers=[
-            log_handler,
-        ],
-        **kwargs,
-    )
+        rich_formatter = logging.Formatter(
+            datefmt="[%Y-%m-%d %H:%M:%S]", fmt="%(message)s"
+        )
+        logging.root.handlers[0].setFormatter(rich_formatter)
+        level_to_log = logging.NOTSET
+    logging.root.setLevel(level_to_log)
