44d26382a1aab40c695b6523ad4c16428ef1ba18
Justin Flannery <juftin@juftin.com>
2023-04-22 11:42:39 -0600

    👷 semantic-release refactor

---
+++ .releaserc.js
@@ -0,0 +1,74 @@
+const path = require("path");
+const fs = require("fs");
+
+const semantic_release_dir = path.resolve(__dirname, ".github/semantic_release");
+const release_note_path = path.join(semantic_release_dir, "release_notes.hbs");
+const release_note_template = fs.readFileSync(release_note_path, "utf-8");
+
+module.exports = {
+    branches: [
+        "main",
+        "master",
+        "next",
+        "next-major",
+        "+([0-9])?(.{+([0-9]),x}).x",
+        {
+            name: "beta",
+            prerelease: true,
+        },
+        {
+            name: "alpha",
+            prerelease: true,
+        },
+    ],
+    plugins: [
+        [
+            "semantic-release-gitmoji",
+            {
+                releaseNotes: {
+                    template: release_note_template,
+                },
+            },
+        ],
+        [
+            "@semantic-release/exec",
+            {
+                prepareCmd: "poetry version ${nextRelease.version} && poetry build",
+            },
+        ],
+        [
+            "@google/semantic-release-replace-plugin",
+            {
+                replacements: [
+                    {
+                        files: ["*/_version.py"],
+                        ignore: ["tests/*"],
+                        from: '__version__ = ".*"',
+                        to: '__version__ = "${nextRelease.version}"',
+                    },
+                ],
+            },
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["pyproject.toml", "*/_version.py"],
+                message:
+                    "🔖 camply ${nextRelease.version}\n\n${nextRelease.notes}\n[skip ci]",
+            },
+        ],
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    {
+                        path: "dist/*.whl",
+                    },
+                    {
+                        path: "dist/*.tar.gz",
+                    },
+                ],
+            },
+        ],
+    ],
+};
