2c5b6575f0d868326ae133a2bb35bd5bb30b754c
Justin Flannery <juftin@juftin.com>
2023-06-04 12:06:39 -0600

    ✨ Minnesota State Parks

--- tests/cli/test_usedirect.py
+++ tests/cli/test_usedirect.py
@@ -13,7 +13,45 @@ from tests.conftest import CamplyRunner, cli_status_checker, vcr_cassette
 
 
 @vcr_cassette
-@mock.patch.dict(os.environ, {"CAMPLY_PROVIDER": "OregonMetro"})
+@mock.patch.dict(os.environ, {"CAMPLY_PROVIDER": OregonMetro.__name__})
+def test_oregon_metro_recreation_areas(
+    cli_runner: CamplyRunner, tmp_path: pathlib.Path, monkeypatch: MonkeyPatch
+) -> None:
+    """
+    CLI Testing - OregonMetro recreation areas
+    """
+    monkeypatch.setattr(OregonMetro, "offline_cache_dir", tmp_path)
+    test_command = """
+    camply recreation-areas \
+     --search "Park"
+    """
+    result = cli_runner.run_camply_command(test_command)
+    cli_status_checker(result=result, exit_code_zero=True)
+    assert 'Using Camply Provider: "OregonMetro"' in result.output
+    assert "Oxbow Regional Park" in result.output
+
+
+@vcr_cassette
+@mock.patch.dict(os.environ, {"CAMPLY_PROVIDER": OregonMetro.__name__})
+def test_oregon_metro_campgrounds(
+    cli_runner: CamplyRunner, tmp_path: pathlib.Path, monkeypatch: MonkeyPatch
+) -> None:
+    """
+    CLI Testing - OregonMetro campgrounds
+    """
+    monkeypatch.setattr(OregonMetro, "offline_cache_dir", tmp_path)
+    test_command = """
+    camply campgrounds \
+     --search "Park"
+    """
+    result = cli_runner.run_camply_command(test_command)
+    cli_status_checker(result=result, exit_code_zero=True)
+    assert 'Using Camply Provider: "OregonMetro"' in result.output
+    assert "Oxbow Regional Park Campground" in result.output
+
+
+@vcr_cassette
+@mock.patch.dict(os.environ, {"CAMPLY_PROVIDER": OregonMetro.__name__})
 def test_oregon_metro_campsites(
     cli_runner: CamplyRunner, tmp_path: pathlib.Path, monkeypatch: MonkeyPatch
 ) -> None:
