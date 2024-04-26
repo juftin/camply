d400366e7f5f8cb27e7306359f597737af0c2223
Dan Wager <danielwager@gmail.com>
2024-04-26 14:34:59 -0700

    🐛 add accept-language header to goingtocamp requests

--- camply/providers/going_to_camp/going_to_camp_provider.py
+++ camply/providers/going_to_camp/going_to_camp_provider.py
@@ -335,8 +335,11 @@ class GoingToCamp(BaseProvider):
         url = None
         if endpoint:
             url = endpoint.format(hostname)
-        user_agent = {"User-Agent": UserAgent(browsers=["chrome"]).random}
-        response = requests.get(url=url, headers=user_agent, params=params, timeout=30)
+        headers = {
+            "User-Agent": UserAgent(browsers=["chrome"]).random,
+            "Accept-Language": "en-US,en;q=0.9",
+        }
+        response = requests.get(url=url, headers=headers, params=params, timeout=30)
         if response.ok is False:
             error_message = f"Receiving bad data from GoingToCamp API: status_code: {response.status_code}: {response.text}"
             logger.error(error_message)
